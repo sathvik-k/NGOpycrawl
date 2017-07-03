2c79f57fe8081353be516e262c02a13e2c956256
Sathvik Koneru <sathvik@koneru.org>
2017-07-03 09:53:46 -0700

    adding address search

--- NGOcrawler/spiders/spider_base.py
+++ NGOcrawler/spiders/spider_base.py
@@ -21,6 +21,10 @@ class NGOSpider(scrapy.Spider):
             f.write(response.body)
         self.log('Saved file %s' % filename)
 
+        print('_______________________________________________________________')
+        print('SCRAPED DATA:')
+        print('_______________________________________________________________')
+        print('_______________________________________________________________')
 
         title = response.xpath('//title/text()').extract_first()
         print title
@@ -41,6 +45,12 @@ class NGOSpider(scrapy.Spider):
         for x in parsedHTML:
             phoneNumber.append(self.getPhoneNumber(x))
             email.append(self.getEmail(x))
+            streetAddress.append(self.getAddress(x))
+
+        print('_______________________________________________________________')
+        print('_______________________________________________________________')
+
+
 
     #getting all text on a page
     def getHTMLtext (self, url):
@@ -80,6 +90,58 @@ class NGOSpider(scrapy.Spider):
         email = re.findall(emailCombinations, webText)[0]
         print(email)
         return email
+
+
+    #using alg
+    def getAddress(self, webText):
+
+        streetNumber = r'\d+'
+        stateAbbrev = r'(AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT \
+                |NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY)?'
+        state = r'(Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii| \
+                Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|\
+                Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New[ ]Hampshire|New[ ]Jersey|New[ ]Mexico|\
+                New[ ]York|North[ ]Carolina|North[ ]Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode[ ]Island|\
+                South[ ]Carolina|South[ ]Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West[ ]Virginia|Wisconsin|Wyoming)?'
+        zipCode = r'[ ]+(\b\d{5}(?:-\d{4})?\b)'
+
+
+        addressBenchmark = stateAbbrev + state + zipCode
+
+        #find address
+        address = re.findall(addressBenchmark, webText)
+
+        stringReform = ''
+        index = 0
+        addressList = ['','']
+        #append string
+        for (a,b,c) in address:
+            stringReform = a + b + ' ' + c
+            addressList[index] = re.sub("^u'(.*)'$",r'\1',stringReform)
+            streetNum = c
+            index += 1
+
+        #print addresses
+        print addressList
+
+        #find position of zipcode
+        for x in addressList:
+            numberEndLoc = webText.find(streetNum) + len(x) + 1
+            print numberEndLoc
+
+            #print (webText.find(streetNum))
+
+            #assign starting point for looking for street number
+            startSearch = numberEndLoc - 25
+            neededText = webText[startSearch:]
+            #print neededText
+            street_number = re.findall(streetNumber,neededText)
+
+            print street_number
+
+            addressStart = webText.find(streetNumber,realAddress[0])
+            print(webText[addressStart:numberEndLoc])
+
 
 
     def formatJSON (self,title,phoneNumber,emailAddress,streetAddress,projectProposal):
